91adf41db77fae49d6f2686a38f49e0d76bf217b
Francis Dortort <francis@dortort.com>
2015-09-30 20:46:08 +0200

    data store & abstract model class start

---
+++ src/ModelAbstract.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Data;
+
+abstract class ModelAbstract
+{
+    public function getAttributes()
+    {
+        return [];
+    }
+
+    public function getRelationships()
+    {
+        return [];
+    }
+}
---
+++ src/Store.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Data;
+
+class Store
+{
+    public function find($modelName, $id, $options)
+    {
+        // return $model;
+    }
+
+    public function query($modelName, $query) 
+    {
+        // return $cursor;
+    }
+}
