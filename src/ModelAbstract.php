f28fbf8cec478f0440b9716aeea5e5fb4f62ec08
Francis Dortort <francis@dortort.com>
2015-10-02 13:56:03 +0200

    model getter/ setter

--- src/ModelAbstract.php
+++ src/ModelAbstract.php
@@ -13,4 +13,31 @@ abstract class ModelAbstract implements ModelInterface
     {
         return [];
     }
+
+    public function get($key)
+    {
+
+    }
+
+    public function set($key, $value)
+    {
+        return $this;
+    }
+
+    public function __get($key)
+    {
+        $key = $this->inflectKey($key);
+        $this->get($key);
+    }
+
+    public function __set($key, $value)
+    {
+        $key = $this->inflectKey($key);
+        $this->set($key, $value);
+    }
+
+    protected function inflectKey($key)
+    {
+        return $key;
+    }
 }
--- src/ModelInterface.php
+++ src/ModelInterface.php
@@ -4,6 +4,10 @@ namespace Data;
 
 interface ModelInterface
 {
+    public function get($key);
+
+    public function set($key, $value);
+
     public function getAttributes();
 
     public function getRelationships();
